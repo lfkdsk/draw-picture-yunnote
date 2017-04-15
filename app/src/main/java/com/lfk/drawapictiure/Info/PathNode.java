604320924d9c0b995f3b553da8008754f12aa151
lfkdsk <lfk_dsk@hotmail.com>
2017-04-15 16:35:32 +0800

    merge msg

--- app/src/main/java/com/lfk/drawapictiure/Info/PathNode.java
+++ app/src/main/java/com/lfk/drawapictiure/Info/PathNode.java
@@ -1,14 +1,19 @@
 package com.lfk.drawapictiure.Info;
+
 import android.app.Application;
 
 import java.util.ArrayList;
 
+import im.fir.sdk.FIR;
+
 /**
  * Created by liufengkai on 15/8/25.
  */
-public class PathNode extends Application{
-    public class Node{
-        public Node() {}
+public class PathNode extends Application {
+    public class Node {
+        public Node() {
+        }
+
         public float x;
         public float y;
         public int PenColor;
@@ -19,6 +24,7 @@ public class PathNode extends Application{
         public int EraserWidth;
 
     }
+
     private ArrayList<Node> PathList;
 
 
@@ -26,39 +32,40 @@ public class PathNode extends Application{
         return PathList;
     }
 
-    public void addNode(Node node){
+    public void addNode(Node node) {
         PathList.add(node);
     }
 
-    public Node NewAnode(){
+    public Node NewAnode() {
         return new Node();
     }
 
 
-    public void clearList(){
+    public void clearList() {
         PathList.clear();
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        PathList = new ArrayList<Node>();
+        FIR.init(this);
+        PathList = new ArrayList<>();
     }
 
     public void setPathList(ArrayList<Node> pathList) {
         PathList = pathList;
     }
 
-    public Node getTheLastNote(){
-        return PathList.get(PathList.size()-1);
+    public Node getTheLastNote() {
+        return PathList.get(PathList.size() - 1);
     }
 
-    public void deleteTheLastNote(){
-        PathList.remove(PathList.size()-1);
+    public void deleteTheLastNote() {
+        PathList.remove(PathList.size() - 1);
     }
 
     public PathNode() {
-        PathList = new ArrayList<Node>();
+        PathList = new ArrayList<>();
     }
 
 }
